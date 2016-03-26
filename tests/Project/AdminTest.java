30758c6f2a6b23d35f84578f8c7c3c81308ce838
Haylie Murphey <hgmurphey@csbsju.edu>
2016-03-25 21:14:40 -0500

    AdminTest class complete, all basic test methods

--- tests/Project/AdminTest.java
+++ tests/Project/AdminTest.java
@@ -2,108 +2,139 @@ package Project;
 
 import static org.junit.Assert.*;
 
+import org.junit.Before;
 import org.junit.Test;
+
 
 public class AdminTest {
 
+	
+private Admin admin;
+	
+	@Before
+	public void setUp() throws Exception {
+		admin = new Admin("Haylie", "Murphey", "hgmurphey", "password", 'a', 'Y');
+	}
+	
 	@Test
-	public void testIsLoggedOn() {
-		fail("Not yet implemented");
+	public void testIsLoggedOn_NotLogged() {
+		boolean expRes = false;
+		boolean res = admin.isLoggedOn();
+		assertEquals("User is not logged on", expRes, res);
 	}
 
 	@Test
 	public void testLogOn() {
-		fail("Not yet implemented");
+		boolean expRes = true;
+		admin.logOn();
+		boolean res = admin.isLoggedOn();
+		assertEquals("User is logged on", expRes, res);
 	}
 
 	@Test
 	public void testLogOff() {
-		fail("Not yet implemented");
+		boolean expRes = false;
+		admin.logOff();
+		boolean res = admin.isLoggedOn();
+		assertEquals("User is not logged on", expRes, res);
 	}
 
 	@Test
 	public void testGetFirstName() {
-		fail("Not yet implemented");
+		String expRes = "Haylie";
+		String result = admin.getFirstName();
+		assertEquals("First name is " + expRes,expRes, result);
 	}
 
 	@Test
 	public void testSetFirstName() {
-		fail("Not yet implemented");
+		String name = "Ellen";
+		admin.setFirstName("Ellen");
+		String result = admin.getFirstName();
+		assertEquals("First name is now " + name,name, result);
 	}
 
 	@Test
 	public void testGetLastName() {
-		fail("Not yet implemented");
+		String expRes = "Murphey";
+		String result = admin.getLastName();
+		assertEquals("Last name is " + expRes,expRes, result);
 	}
 
 	@Test
 	public void testSetLastName() {
-		fail("Not yet implemented");
+		String expRes = "Stensrud";
+		admin.setLastName("Stensrud");
+		String result = admin.getLastName();
+		assertEquals("Last name is now " + expRes,expRes, result);
 	}
 
 	@Test
 	public void testGetUsername() {
-		fail("Not yet implemented");
+		String expRes = "hgmurphey";
+		String result = admin.getUsername();
+		assertEquals("Username is " + expRes,expRes, result);
 	}
 
 	@Test
 	public void testSetUsername() {
-		fail("Not yet implemented");
+		String expRes = "murphhay";
+		admin.setUsername("murphhay");
+		String result = admin.getUsername();
+		assertEquals("Username is now " + expRes,expRes, result);
 	}
 
 	@Test
 	public void testGetPassword() {
-		fail("Not yet implemented");
+		String expRes = "password";
+		String result = admin.getPassword();
+		assertEquals("Password is" + expRes,expRes, result);
 	}
 
 	@Test
 	public void testSetPassword() {
-		fail("Not yet implemented");
+		String expRes = "12345";
+		admin.setPassword("12345");
+		String result = admin.getPassword();
+		assertEquals("Password is now" + expRes,expRes, result);
 	}
 
 	@Test
 	public void testGetType() {
-		fail("Not yet implemented");
+		char expRes = 'a';
+		char result = admin.getType();
+		assertEquals("Type is " + expRes,expRes, result);
 	}
 
 	@Test
 	public void testSetType() {
-		fail("Not yet implemented");
+		char expRes = 'u';
+		admin.setType('u');
+		char result = admin.getType();
+		assertEquals("Type is now" + expRes,expRes, result);
 	}
 
 	@Test
 	public void testGetStatus() {
-		fail("Not yet implemented");
+		char expRes = 'Y';
+		char result = admin.getStatus();
+		assertEquals("Status is " + expRes,expRes, result);
 	}
 
 	@Test
 	public void testSetStatus() {
-		fail("Not yet implemented");
+		char expRes = 'N';
+		admin.setStatus('N');
+		char result = admin.getStatus();
+		assertEquals("Type is now " + expRes,expRes, result);
 	}
 
 	@Test
 	public void testToString() {
-		fail("Not yet implemented");
+		String expRes = "Admin [firstName=Haylie, lastName()=Murphey, username()=hgmurphey, password()=password, type()=a, status()=Y]";
+		String res = admin.toString();
+		assertEquals("To string is: " + expRes, expRes, res);
 	}
 
-	@Test
-	public void testAdmin() {
-		fail("Not yet implemented");
-	}
-
-	@Test
-	public void testAdminStringStringStringStringCharChar() {
-		fail("Not yet implemented");
-	}
-
-	@Test
-	public void testAccount() {
-		fail("Not yet implemented");
-	}
-
-	@Test
-	public void testAccountStringStringStringStringCharChar() {
-		fail("Not yet implemented");
-	}
 
 }
